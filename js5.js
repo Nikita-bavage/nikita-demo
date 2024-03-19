cf3d646853cdcc3a1a9315e3e63531a5999a4088
nikita-bavage <nikitabavage1415@gmail.com>
2024-03-19 18:08:26 +0530

    todays practice

---
+++ js5.js
@@ -0,0 +1,37 @@
+let newbtn=document.createElement("button");
+newbtn.innerText="clickme!";
+newbtn.style.backgroundColor="red";
+newbtn.style.color="white";
+
+document.querySelector("body").prepend("newbtn");
+
+
+let div=document.querySelector("div");
+//div.append(newbtn);
+//div.prepend(newbtn);
+div.before(newbtn);
+
+
+let heading=document.createElement("h1");
+heading.innerHTML="<i>hello this heading added in JS</i>";
+console.log(heading);
+
+document.querySelector("body").prepend(heading);
+
+
+
+
+let id=div.getAttribute("id");
+console.log(id);
+let name=div.getAttribute("name");
+console.log(name);
+
+let paraa=document.querySelector("p");
+console.log(paraa.setAttribute("class","nikita"));
+
+let paraaa=paraa.getAttribute("class");
+console.log(paraaa);
+
+let change=paraa.setAttribute("class","nikita");
+console.log(change);
+
